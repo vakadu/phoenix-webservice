fc5f8a50ee796e8cea3d7b14b5e05a019110da62
pemilytech <vakadu10@gmail.com>
2024-04-30 17:17:13 +0530

    updaetd new change

--- apps/pemily.com/components/layout/footer.tsx
+++ apps/pemily.com/components/layout/footer.tsx
@@ -1,68 +1,93 @@
-import Link from "next/link";
+import Link from 'next/link';
 import Image from 'next/image';
 
 const Footer = () => {
-    return (
-        <footer className="bg-black-bg px-16 py-24 lg:py-[64px] lg:px-[165px]">
-            <section className="grid lg:grid-cols-3">
-                <section className="col-span-1">
-                    <Link href='/'>
-                        <figure className='w-[93px] h-[38px] relative'>
-                            <Image
-                                fill
-                                src='https://pemilyy-assets.s3.ap-south-1.amazonaws.com/logos-new/logo-white-1.png'
-                                alt='pemilyy'
-                                style={{
-                                    objectFit: 'contain'
-                                }}
-                            />
-                        </figure>
-                    </Link>
-                    <p className="pt-16 pb-32 lg:pt-12 lg:pb-24 text-14 leading-20 text-white">Copyright © {new Date().getFullYear()} Veena Tech.</p>
-                </section>
-                <section className="col-span-2 grid grid-cols-2 lg:grid-cols-3 gap-24 text-white">
-                    <section className="col-span-1">
-                        <p className="text-20 font-semibold leading-28 mb-10 lg:mb-28">Company</p>
-                        <ul>
-                            <li className="py-8">
-                                <Link className='text-14 leading-20 lg:text-16 lg:leading-24' href='/about'>
-                                    About Us
-                                </Link>
-                            </li>
-                            <li className="py-8">
-                                <Link className='text-14 leading-20 lg:text-16 lg:leading-24' href='/about'>
-                                    Services
-                                </Link>
-                            </li>
-                            <li className="py-8">
-                                <Link className='text-14 leading-20 lg:text-16 lg:leading-24' href='/about'>
-                                    Why Pemily
-                                </Link>
-                            </li>
-                        </ul>
-                    </section>
-                    <section className="col-span-1">
-                        <p className="text-20 font-semibold leading-28 mb-10 lg:mb-28">Support</p>
-                        <ul>
-                            <li className="py-8">
-                                <Link className='text-14 leading-20 lg:text-16 lg:leading-24' href='/about'>
-                                    Help center
-                                </Link>
-                            </li>
-                            <li className="py-8">
-                                <Link className='text-14 leading-20 lg:text-16 lg:leading-24' href='/about'>
-                                    Terms of service
-                                </Link>
-                            </li>
-                        </ul>
-                    </section>
-                    <section className="col-span-1">
-                        <p className="text-20 font-semibold leading-28">Stay up to date</p>
-                    </section>
-                </section>
-            </section>
-        </footer>
-    )
+	return (
+		<footer className="bg-black-bg px-16 py-24 lg:py-[64px] lg:px-[165px]">
+			<section className="grid lg:grid-cols-3">
+				<section className="col-span-1">
+					<Link href="/">
+						<figure className="w-[93px] h-[38px] relative">
+							<Image
+								fill
+								src="https://pemilyy-assets.s3.ap-south-1.amazonaws.com/logos-new/logo-white-1.png"
+								alt="pemilyy"
+								style={{
+									objectFit: 'contain',
+								}}
+							/>
+						</figure>
+					</Link>
+					<p className="pt-16 pb-32 lg:pt-12 lg:pb-24 text-14 leading-20 text-white">
+						Copyright © {new Date().getFullYear()} Veena Tech.
+					</p>
+				</section>
+				<section className="col-span-2 grid grid-cols-2 lg:grid-cols-3 gap-24 text-white">
+					<section className="col-span-1">
+						<p className="text-20 font-semibold leading-28 mb-10 lg:mb-28">Company</p>
+						<ul>
+							<li className="py-8">
+								<Link
+									className="text-14 leading-20 lg:text-16 lg:leading-24"
+									href="/about"
+								>
+									About Us
+								</Link>
+							</li>
+							<li className="py-8">
+								<Link
+									className="text-14 leading-20 lg:text-16 lg:leading-24"
+									href="/"
+								>
+									Services
+								</Link>
+							</li>
+							<li className="py-8">
+								<Link
+									className="text-14 leading-20 lg:text-16 lg:leading-24"
+									href="/"
+								>
+									Why Pemily
+								</Link>
+							</li>
+						</ul>
+					</section>
+					<section className="col-span-1">
+						<p className="text-20 font-semibold leading-28 mb-10 lg:mb-28">Support</p>
+						<ul>
+							<li className="py-8">
+								<Link
+									className="text-14 leading-20 lg:text-16 lg:leading-24"
+									href="/privacy"
+								>
+									Privacy Policy
+								</Link>
+							</li>
+							<li className="py-8">
+								<Link
+									className="text-14 leading-20 lg:text-16 lg:leading-24"
+									href="/terms"
+								>
+									Terms of service
+								</Link>
+							</li>
+							<li className="py-8">
+								<Link
+									className="text-14 leading-20 lg:text-16 lg:leading-24"
+									href="/refund-policy"
+								>
+									Refund Policy
+								</Link>
+							</li>
+						</ul>
+					</section>
+					<section className="col-span-1">
+						<p className="text-20 font-semibold leading-28">Stay up to date</p>
+					</section>
+				</section>
+			</section>
+		</footer>
+	);
 };
 
 export default Footer;
--- apps/pemily.com/components/layout/menu.tsx
+++ apps/pemily.com/components/layout/menu.tsx
@@ -3,39 +3,51 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const Menu = () => {
-    return (
-        <section className="px-16 lg:px-0 flex flex-col lg:flex-row h-full">
-            <section className='flex-1 gap-24 lg:gap-[60px] items-start flex flex-col lg:flex-row lg:items-center lg:justify-center'>
-                <Link className='my-32 lg:hidden' href='/'>
-                    <figure className='w-[93px] h-[38px] relative'>
-                        <Image
-                            fill
-                            src='https://pemilyy-assets.s3.ap-south-1.amazonaws.com/logos-new/logo-primary.png'
-                            alt='pemilyy'
-                            style={{
-                                objectFit: 'contain'
-                            }}
-                        />
-                    </figure>
-                </Link>
-                <Link className='text-18 hover:text-brand lg:text-16 lg:leading-24 block lg:inline-block lg:w-auto w-full' href='/about'>
-                    About Us
-                </Link>
-                <Link className='text-18 hover:text-brand lg:text-16 lg:leading-24 lg:inline-block lg:w-auto block w-full' href='/about'>
-                    Services
-                </Link>
-                <Link className='text-18 hover:text-brand lg:text-16 lg:leading-24 lg:inline-block lg:w-auto block w-full' href='/about'>
-                    Why Pemily
-                </Link>
-                <Link className='text-18 hover:text-brand lg:text-16 lg:leading-24 lg:w-auto block w-full lg:hidden' href='/about'>
-                    Contact Us
-                </Link>
-            </section>
-            <Link className='btn btn-primary btn-medium hidden lg:block' href='/'>
-                Contact Us
-            </Link>
-        </section>
-    );
+	return (
+		<section className="px-16 lg:px-0 flex flex-col lg:flex-row h-full">
+			<section className="flex-1 gap-24 lg:gap-[60px] items-start flex flex-col lg:flex-row lg:items-center lg:justify-center">
+				<Link className="my-32 lg:hidden" href="/">
+					<figure className="w-[93px] h-[38px] relative">
+						<Image
+							fill
+							src="https://pemilyy-assets.s3.ap-south-1.amazonaws.com/logos-new/logo-primary.png"
+							alt="pemilyy"
+							style={{
+								objectFit: 'contain',
+							}}
+						/>
+					</figure>
+				</Link>
+				<Link
+					className="text-18 hover:text-brand lg:text-16 lg:leading-24 block lg:inline-block lg:w-auto w-full"
+					href="/"
+				>
+					About Us
+				</Link>
+				<Link
+					className="text-18 hover:text-brand lg:text-16 lg:leading-24 lg:inline-block lg:w-auto block w-full"
+					href="/"
+				>
+					Services
+				</Link>
+				<Link
+					className="text-18 hover:text-brand lg:text-16 lg:leading-24 lg:inline-block lg:w-auto block w-full"
+					href="/"
+				>
+					Why Pemily
+				</Link>
+				<Link
+					className="text-18 hover:text-brand lg:text-16 lg:leading-24 lg:w-auto block w-full lg:hidden"
+					href="/about"
+				>
+					Contact Us
+				</Link>
+			</section>
+			<Link className="btn btn-primary btn-medium hidden lg:block" href="/">
+				Contact Us
+			</Link>
+		</section>
+	);
 };
 
 export default Menu;
