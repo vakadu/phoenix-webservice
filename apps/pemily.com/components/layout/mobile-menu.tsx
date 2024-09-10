5e3183f36b695ee11946194830362c4b504d6f99
Vakadu Vinod <vakadu10@gmail.com>
2024-09-10 14:56:21 +0530

    fixes

--- apps/pemily.com/components/layout/mobile-menu.tsx
+++ apps/pemily.com/components/layout/mobile-menu.tsx
@@ -4,20 +4,20 @@ import { Modal } from '@webservices/ui';
 import Menu from './menu';
 
 interface IMobileMenupProps {
-    mobileMenu: boolean;
-    setMobileMenu: (m: boolean) => void;
+	mobileMenu: boolean;
+	setMobileMenu: (m: boolean) => void;
 }
 
 const MobileMenu = (props: IMobileMenupProps) => {
-    const { mobileMenu, setMobileMenu } = props;
+	const { mobileMenu, setMobileMenu } = props;
 
-    return (
-        <Modal isCenter isOpen={mobileMenu} handleClose={() => setMobileMenu(false)}>
-            <section className='relative z-50 bg-white h-screen w-[75%] inline-block align-middle'>
-                <Menu/>
-            </section> 
-        </Modal>
-    )
+	return (
+		<Modal isOpen={mobileMenu} handleClose={() => setMobileMenu(false)}>
+			<section className="relative z-50 bg-white h-screen w-[75%] inline-block align-middle">
+				<Menu />
+			</section>
+		</Modal>
+	);
 };
 
 export default MobileMenu;
