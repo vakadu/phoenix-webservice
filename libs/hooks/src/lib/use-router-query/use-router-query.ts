4c223ed34ab234014f5f1446a94d190ab8c919c5
Vakadu Vinod <vakadu10@gmail.com>
2024-09-25 09:54:04 +0530

    Update use-router-query.ts

--- libs/hooks/src/lib/use-router-query/use-router-query.ts
+++ libs/hooks/src/lib/use-router-query/use-router-query.ts
@@ -12,13 +12,22 @@ export interface UseRouterQuery {
 
 export function useRouterQuery() {
 	const router = useRouter() as any;
+	const searchParams = useSearchParams();
+	const pathName = usePathname();
+
+	const updateQueryParams = ({ key, value }: { key: string; value: string }) => {
+		const currentParams = new URLSearchParams(searchParams.toString());
+		currentParams.set(key, value);
+		router.replace(`${pathName}?${currentParams.toString()}`, { scroll: true });
+	};
 
 	return {
 		query: useParams(),
 		router,
 		back: router.back,
-		pathname: usePathname(),
-		params: useSearchParams(),
+		pathname: pathName,
+		params: searchParams,
+		updateQueryParams,
 	};
 }
 
