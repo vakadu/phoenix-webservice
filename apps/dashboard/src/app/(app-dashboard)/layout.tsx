c7307ea8887d92c212ae2f0ca59d682d74e7c7e8
Vakadu Vinod <vakadu10@gmail.com>
2024-10-09 15:21:51 +0530

    added frmaer motion

--- apps/dashboard/src/app/(app-dashboard)/layout.tsx
+++ apps/dashboard/src/app/(app-dashboard)/layout.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useMemo } from 'react';
 import { useSelector } from 'react-redux';
+import { motion, AnimatePresence } from 'framer-motion';
 
 import { PemilyRootState } from '@webservices/slices';
 import { useIsMobile, useRouterQuery } from '@webservices/hooks';
@@ -10,7 +11,7 @@ import { DashboardLayoutHeader, DashboardLayoutSidebar } from '@webservices/ui';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 	const authState = useSelector((state: PemilyRootState) => state.auth);
-	const { router } = useRouterQuery();
+	const { router, pathname } = useRouterQuery();
 	const { isDesktop } = useIsMobile();
 
 	useEffect(() => {
@@ -31,7 +32,33 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 		<section className="max-w-screen-2xl">
 			<DashboardLayoutHeader />
 			<DashboardLayoutSidebar />
-			<div className={`${sidebarClasses} min-h-[calc(100vh-72px)]`}>{children}</div>
+			<motion.div
+				key={pathname}
+				initial="pageInitial"
+				animate="pageAnimate"
+				exit="pageExit"
+				variants={{
+					pageInitial: {
+						opacity: 0,
+						y: 50,
+					},
+					pageAnimate: {
+						opacity: 1,
+						y: 0,
+					},
+					pageExit: {
+						opacity: 0,
+						y: -50,
+					},
+				}}
+				transition={{
+					type: 'tween',
+					ease: 'easeInOut',
+					duration: 0.5,
+				}}
+			>
+				<div className={`${sidebarClasses} min-h-[calc(100vh-72px)]`}>{children}</div>
+			</motion.div>
 		</section>
 	);
 };
