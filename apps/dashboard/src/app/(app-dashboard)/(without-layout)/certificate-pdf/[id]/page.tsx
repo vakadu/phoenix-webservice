1f5e4c07daa0f95579442b807b605cfca5f7c594
Avishek Singh <avishek@Avisheks-MacBook-Pro.local>
2024-11-17 16:09:50 +0530

    tets

--- apps/dashboard/src/app/(app-dashboard)/(without-layout)/certificate-pdf/[id]/page.tsx
+++ apps/dashboard/src/app/(app-dashboard)/(without-layout)/certificate-pdf/[id]/page.tsx
@@ -5,6 +5,7 @@ const Page = () => {
 	return (
 		<div className="pb-[100px]">
 			<div className="my-6 font-bold text-18 text-purple text-center underline decoration-purple">PREVIEW</div>
+			<div className="mb-10 font-medium text-14 text-purple text-center"><span className='font-bold'>NOTE: </span>VERIFY DOB AND AGE BEFORE PRINTING. UPDATE IN PET PROFILE IF INCORRECT, THEN RECREATE CERTIFICATE.</div>
 			<CertificatePdf />
 			<Print />
 		</div>
